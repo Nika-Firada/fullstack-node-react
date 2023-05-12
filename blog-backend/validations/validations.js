bfa9249631532219181395f50830d98cde017201
Nika-Firada <94400714+Nika-Firada@users.noreply.github.com>
2023-05-13 00:24:54 +0400

    Update validations.js

--- blog-backend/validations/validations.js
+++ blog-backend/validations/validations.js
@@ -1,20 +1,20 @@
 import { body } from 'express-validator';
 
 export const loginValidation = [
-  body('email', 'Неверный формат почты').isEmail(),
-  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
+  body('email', 'wrong email').isEmail(),
+  body('password', 'password must be more than 5 char').isLength({ min: 5 }),
 ];
 
 export const registerValidation = [
-  body('email', 'Неверный формат почты').isEmail(),
-  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
-  body('fullName', 'Укажите имя').isLength({ min: 3 }),
-  body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
+  body('email', 'wrong email').isEmail(),
+  body('password', 'password must be more than 5 char').isLength({ min: 5 }),
+  body('fullName', 'write name').isLength({ min: 3 }),
+  body('avatarUrl', 'error url').optional().isURL(),
 ];
 
 export const postCreateValidation = [
-  body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
-  body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
-  body('tags', 'Неверный формат тэгов').optional(),
-  body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+  body('title', 'enter title').isLength({ min: 3 }).isString(),
+  body('text', 'enter text').isLength({ min: 3 }).isString(),
+  body('tags', 'error tags').optional(),
+  body('imageUrl', 'error url').optional().isString(),
 ];
